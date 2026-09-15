138a7cd0fd76e2630b0fdfa3b0db84d1dcb7ffb1
aliyyulmunifbebas <rexixstore@gmail.com>
2026-09-15 16:24:58 +0700

    mengubah data wa

--- data/store.js
+++ data/store.js
@@ -3,7 +3,7 @@ export const store = {
   city: "Semarang",
   map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.1774072874546!2d110.40042417499713!3d-6.988373293012596!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e708b4785712c87%3A0x59a2c5ebbc30b963!2sAbon%20Sewa%20Kamera%20Semarang%20(Lucky%20Space%20Semarang)!5e0!3m2!1sen!2sid!4v1789380692782!5m2!1sen!2sid",
   instagram: "abonkamerasemarang",
-  whatsapp: "6282222207157",
+  whatsapp: "6282329475745",
   websiteUrl: "https://abon-kamera-demo.vercel.app",
   address: "Jl. Pemuda No. 123, Sekayu, Semarang Tengah",
   day: "Sabtu - Minggu",
